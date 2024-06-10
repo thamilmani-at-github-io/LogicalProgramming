3af10a6a2c6c975e9c49fe45d58f23cee16b8c55
THAMILMANI VARATHARAJAN <v.kishoremani@gmail.com>
2024-06-10 16:47:25 +0530

    left side smaller

---
+++ Java/leftsideSmaller.java
@@ -0,0 +1,17 @@
+class leftsideSmaller{
+    public static void main(String[] args) {
+        int[] a = new int[]{4,2,5,7};
+        int n = a.length-1;
+        int temp = a[0];
+        for(int i = 1;i<n;i++){
+            if(a[i]>temp){
+                System.out.println(a[i]);
+                temp = a[i];
+            }
+            else{
+                //System.out.println(a[i]);
+                temp = a[i];
+            }
+        }
+    }
+}
